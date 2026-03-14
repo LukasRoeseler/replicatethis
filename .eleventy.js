65c2bff8422d13de835b391ede274dcbb076bde6
LukasRoeseler <lukas.roeseler@uni-muenster.de>
2026-03-14 10:19:42 +0100

    Update for gh pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,8 @@
-
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   
   return {
+    pathPrefix: process.env.PATH_PREFIX || "/", // Helps GitHub Pages find assets
     dir: {
       input: "src",
       output: "_site"
