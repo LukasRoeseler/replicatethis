c783f657ad29662b6951f64cc5074af1659b9de3
Lukas Röseler <117925406+LukasRoeseler@users.noreply.github.com>
2026-03-14 18:51:43 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy(".nojekyll"); // Added this line
   
   return {
-    pathPrefix: process.env.PATH_PREFIX || "/", // Helps GitHub Pages find assets
+    pathPrefix: process.env.PATH_PREFIX || "/",
     dir: {
       input: "src",
       output: "_site"
